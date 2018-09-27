6a52fdeea15cf04b4b701514d997a35ca59a2482
helenahwang <yeonha.hwang@gmail.com>
2018-09-27 18:07:07 +0900

    Upload

---
+++ basicview/src/main/java/com/appdev/a503_02/basicview/TextViewAttr.java
@@ -0,0 +1,59 @@
+package com.appdev.a503_02.basicview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TextViewAttr extends AppCompatActivity {
+
+    //인스턴스 변수 선언
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view_attr);
+
+        editText = (EditText)findViewById(R.id.edit);
+        textView = (TextView)findViewById(R.id.text);
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textView.setText(editText.getText());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });
+
+
+
+
+
+
+        //XML 파일에 만든 뷰를 가져오기
+        TextView tv = (TextView)findViewById(R.id.abc);
+
+        //text 속성을 설정할 때는 String을 대입 가능
+        tv.setText("환영합니다.");
+
+        //가져올 때는 toString()을 이용해서 String으로 변환해서 가져와야 한다.
+        String msg = tv.getText().toString();
+
+    }
+}
